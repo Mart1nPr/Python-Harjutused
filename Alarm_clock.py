6e3939efb10704af827b9daeb27e1ed4b83a3e88
Martin Pruus <martin.pruus@tptlive.ee>
2024-11-27 23:57:19 +0200

    Started Alarm_clock.py

---
+++ Alarm_clock.py
@@ -0,0 +1,25 @@
+# Alarm clock
+# Started: 11/27/2024
+# Finished: 
+
+from datetime import datetime
+
+current_time = datetime.now()
+
+print(current_time.time())
+
+hourSelection = int(input("Enter hour/hours (0-12): "))
+if hourSelection in (0,12):
+    pass
+else:
+    print("Over 12!")
+
+minuteSelection = int(input("Enter minute/minutes (0-59): "))
+if minuteSelection in (0,59):
+    pass
+else:
+    print("Over 59!")
+
+meridiemSelection = input("Enter meridian (AM/PM): ")
+
+
