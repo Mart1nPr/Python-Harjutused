62d37a66e6aed5344dafb0a82a332179d2f3bf74
Martin Pruus <martin.pruus@tptlive.ee>
2024-11-29 12:32:58 +0200

    Added half finished tictactoe.py

---
+++ Tic-Tac-Toe.py
@@ -0,0 +1,19 @@
+# Tic Tac Toe
+
+Grid = [0,0,0,
+        0,0,0,
+        0,0,0]
+
+
+for i in range(9):
+    print(Grid)
+    user1Guess = int(input("Choose where to place (x) on the grid (1-9): "))
+    if user1Guess == 1:
+        Grid.append(0, x)
+    elif user1Guess == 2:
+        Grid.append(1,2)
+    print(Grid)
+    user2Guess = int ("Choose where to place (O) on the grid (1-9): ")
+
+
+
